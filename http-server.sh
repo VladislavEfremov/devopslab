440ddea9c67bf2ea0f5e1caf1c26b9fd3c26be77
Vladislav <Vlad1slav.Efremov@yandex.ru>
2020-08-29 16:44:50 +0300

    Delete http-server.sh

--- http-server.sh
+++
@@ -1,6 +0,0 @@
-sudo apt update
-sudo apt install git -y
-git clone https://github.com/VladislavEfremov/devopslab.git
-sudo apt install apache2 -y
-cd devopslab/
-sudo cp -r example_beebox/. /var/www/html/
