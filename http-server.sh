ccb9d966f13cfd4982aae52d93d8ea70dbc279ab
Vladislav <Vlad1slav.Efremov@yandex.ru>
2020-08-29 10:11:19 +0300

    Update http-server.sh

--- http-server.sh
+++ http-server.sh
@@ -3,4 +3,4 @@ sudo apt install git -y
 git clone https://github.com/VladislavEfremov/devopslab.git
 sudo apt install apache2 -y
 cd devopslab/
-sudo cp -r /example_beebox /var/www/html/
+sudo cp -r example_beebox/. /var/www/html/
